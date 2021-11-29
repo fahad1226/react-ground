2ef07512651bd854b4d08fd33ba6689d8f078002
fahad1226 <fahadbinmnr@gmail.com>
2021-11-29 20:04:47 +0600

    add a simple name hover functionality

---
+++ changeName.js
@@ -0,0 +1,21 @@
+/*
+ * Title: CHange Name Script File
+ * Description: A vanilla JS Implementation of a simple Counter that adds 1 to each user click
+ * Author: Fahad Bin Munir
+ * Date: 29/11/2021
+ *
+ */
+
+let name = "";
+
+let nameHeader = document.querySelector("#name");
+
+nameHeader.addEventListener("mouseenter", () => {
+    name = "Fahad Bin Munir";
+    nameHeader.textContent = name;
+});
+
+nameHeader.addEventListener("mouseleave", () => {
+    name = "Fahad";
+    nameHeader.textContent = name;
+});
